ba54dec2952346e04644e78177744d0ebd2e3223
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2026-01-22 09:44:30 -0300

    Atualiza exportação de símbolos no build.rs para incluir 'calcular_sha1_hash' e remove referências obsoletas

--- build.rs
+++ build.rs
@@ -3,8 +3,7 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "x86" {
         // Força a exportação dos símbolos sem decoração (sem @N)
         // Isso facilita o uso no VB6 sem precisar de Alias complexos
-        println!("cargo:rustc-cdylib-link-arg=/EXPORT:processar_dados_excel_para_mysql");
-        println!("cargo:rustc-cdylib-link-arg=/EXPORT:processar_dados_excel_para_mysql_env");
+        println!("cargo:rustc-cdylib-link-arg=/EXPORT:calcular_sha1_hash");
         println!("cargo:rustc-cdylib-link-arg=/EXPORT:free_string");
     }
 }
